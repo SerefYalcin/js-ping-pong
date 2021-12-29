388a1fa7bd1a50882e8607d88159f8eb7434ecfd
SerefYalcin <serefyalcin07610@gmail.com>
2021-12-29 20:26:42 +0300

    intial

---
+++ Ball.js
@@ -0,0 +1,28 @@
+export default class Ball {
+    constructor(ballElem)   {
+        this.ballElem   = ballElem
+    }
+
+    get x() {
+        return parselFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
+
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value)
+    }
+    get y() {
+        return parselFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    }
+
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value)
+    }
+
+    update(delta) {
+        this.x = 5
+        this.y = 15
+
+    }
+
+
+}
